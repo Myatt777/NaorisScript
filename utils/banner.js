e64f9b563d3adbf79b11a7b35827b560c49d5306
Myatt777 <67530122+Myatt777@users.noreply.github.com>
2025-02-02 20:39:03 +0630

    Update banner.js

--- utils/banner.js
+++ utils/banner.js
@@ -1,1 +1,18 @@
+import chalk from 'chalk';
+
+const banner = () => {
+  const text = `
+  _  ____  __ _   _  ____  
+ | |/ /  \\/  | \\ | |/ __ \\ 
+ | ' /| \\  / |  \\| | |  | |
+ |  < | |\\/| | . \` | |  | |
+ | . \\| |  | | |\\  | |__| |
+ |_|\\_\\_|  |_|_| \\_|\\____/ 
+  `;
+
+  const separator = "═".repeat(60);
+  return `${chalk.cyan(text)}\n${chalk.white(separator)}`;
+};
+
+export default banner;
 
